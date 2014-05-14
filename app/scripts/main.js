aa3d4416337118fd17c3a09f4b1b3b733d79600e
brookebc <brooke.casey@gmail.com>
2014-05-13 22:45:32 -0400

    trying to get my newly concated link to send back results

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	console.log("ready")
+	console.log("ready");
 
 	creeper.init();
 });
@@ -8,47 +8,140 @@ $(document).ready(function() {
 var creeper = {
 
 init: function() {
-// this.initStyling();
+	// this.initStyling();
 	this.initEvents();
 
 },
 
 initStyling: function() {
-	// this.renderYoStuff();
+	// this.seeYoStuff();
 },
 initEvents: function(){
-	$(".searchspot").click(this.seeYoStuff);
+	$(".userdataform").on("submit", this.makeSearchUri);
+	// $(".userdataform").on("submit", this.seeYoStuff);
+	$(".sources").on("click", this.seeSources);
 	
 },
 
-seeYoStuff: function(){
-	console.log("i heard you click the go for it button");
-	
+makeSearchUri: function(e){
+	e.preventDefault;
+
+	var first = $('.first').val();
+	console.log(first);
+
+	var middle = $('.middle').val();
+	console.log(middle);
+
+	var last = $('.last').val();
+	console.log(last);
+
+	var email = $('.email').val();
+	var readyemail =  encodeURIComponent(email);
+	console.log(readyemail);
+
+	var phone = $('.phone').val();
+	console.log(phone);
+
+	var city = $('.city').val();
+	console.log(city);
+
+	var state = $('.state').val();
+	console.log(state);
+
+	var country = $('.country').val();
+	console.log(country);
+
+	var key = "d24xragg5n8recdnvvdffatc";
+
+var readyurl = "http://api.pipl.com/search/v3/json/?" + "&email_name=" + readyemail + "&phone=" + phone + "&first_name=" + first + "&last_name=" + last + "&middle_name=" + middle + "&country=" + country + " &state=" + state + "&callback=JSONP&exact_name=0&query_params_mode=and&key=" + key;
+alert(readyurl);
+
 	$.ajax({
 			type:"GET",
-			url: "http://api.pipl.com/search/v3/json/?email=brooke.casey%40gmail.com&phone=8437258318&first_name=Brooke&last_name=Casey&middle_name=Brittany&raw_name=brooke+casey&country=US&state=SC&from_age=25&to_age=30&callback=JSONP&exact_name=0&query_params_mode=and&key=d24xragg5n8recdnvvdffatc",
+			url: readyurl,
 			dataType: "jsonp",
-			data: $('myForm').serialize(),	// {email: $("#emailholder").val()}
-
 			error: function(jqXHR, status, error){
 				alert("no! something is wrong" + error);
 			},
 			success: function (data, datatype, jqXHR){
 				console.log("success!");
 
-				var records = data.records;
-				console.log(records);
+				var peopleinfo = data.person;
+				console.log(peopleinfo);
+
 				var html = '';
 
-				for (var i = 0; i <records.length; i++){
+				for (var i = 0; i < peopleinfo.addresses.length; i++){
 
-				html += '<li>' + records[i].source.domain + '</li>\n';
+				html += '<li>' + peopleinfo.addresses[i].display + '</li>\n';
 				};
-				$(".righthere").append(html);	
+				console.log(html);
+				$(".righthere").html(html);	
 			}
+		});
+},
 
-	});
+seeSources: function(e){
+	e.preventDefault();
+
+	console.log("so you wanna see my sources");
+	$(".sources").append("Just a sec. Let me get it organized for you");
+
+	$.ajax({
+				type:"GET",
+				url: "http://api.pipl.com/search/v3/json/?email=brooke.casey%40gmail.com&phone=8437258318&first_name=Brooke&last_name=Casey&middle_name=Brittany&raw_name=brooke+casey&country=US&state=SC&from_age=25&to_age=30&callback=JSONP&exact_name=0&query_params_mode=and&key=d24xragg5n8recdnvvdffatc",
+				dataType: "jsonp",
+				error: function(jqXHR, status, error){
+					alert("no! something is wrong" + error);
+				},
+				success: function (data, datatype, jqXHR){
+					console.log("success!");
+
+					var records = data.records;
+	 				console.log(records);
+
+					var html = '';
+	  
+	  				for (var i = 0; i <records.length; i++){
+	  
+	 				html += '<li>' + records[i].source.domain + '</li>\n';
+	 				};
+
+	 				console.log(html);
+	 				$(".sourceshere").html(html);	
+	 			}
+			});
+
+	}
 
 }
 
-}
+// mapYoSelf: function(){
+// 	console.log("i heard you click the go for it button");
+	
+// 	$.ajax({
+// 			type:"GET",
+// 			url: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBozij7O9-LBYqRlEPS5IKUUY9jXvOiKIk&sensor=false",
+// 			dataType: "jsonp",
+// 			error: function(jqXHR, status, error){
+// 				alert("no! something is wrong" + error);
+// 			},
+// 			success: function (data, datatype, jqXHR){
+// 				console.log("success!");
+
+// 				var records = data.records;
+// 				console.log(records);
+// 				var html = '';
+
+// 				for (var i = 0; i <records.length; i++){
+
+// 				html += '<li>' + records[i].source.domain + '</li>\n';
+// 				};
+// 				$(".righthere").append(html);	
+// 			}
+
+// 	});
+
+// }
+
+
---
+++ app/scripts/search.js
@@ -0,0 +1,14 @@
+
+// need to prepare a function that takes the input values from the form to strignify and add to a link to complete the search query
+<p>Example</p>
+"http://api.pipl.com/search/v3/json/?email=brooke.casey%40gmail.com&phone=8437258318&first_name=Brooke&last_name=Casey&middle_name=Brittany&raw_name=brooke+casey&country=US&state=SC&from_age=25&to_age=30&callback=JSONP&exact_name=0&query_params_mode=and&key=d24xragg5n8recdnvvdffatc",
+		
+
+var urilink = "http://api.pipl.com/search/v3/json/?";
+var first = params[first_name].val();
+console.log(first);
+var readylink = urilink+;
+console.log(readylink);
+
+
+<input type="text" name="params[first_name]" value="" placeholder="" />
